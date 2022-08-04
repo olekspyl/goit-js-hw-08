541cf2ffcc6c79f763871c6c46e73bb3420d0673
Oleksandra Pylypchuk <lemocream@gmail.com>
2022-08-04 20:38:57 +0200

    new

---
+++ src/partials/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const imgs = galleryItems
+    .map(galleryItem => {
+        const itemEl = `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+        <img src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" 
+        class="gallery__image"/>
+        </a>
+        </div>` 
+        return itemEl;
+    })
+    .join(" ");
+
+const listEl = document.querySelector(".gallery");
+listEl.insertAdjacentHTML('afterbegin', imgs);
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener('click', onImageClick);
+
+let instance;
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image"))
+        return;
+   
+
+     instance = basicLightbox
+         .create(`<img src="${event.target.dataset.source}" width="800" height="600">`,
+             {onShow: (instance) => {window.addEventListener('keydown', onEscPress)},
+            onClose: (instance) => {window.removeEventListener('keydown', onEscPress)}
+})
+
+instance.show()
+}
+
+galleryContainer.addEventListener('keydown', onEscPress)
+function onEscPress(event) {
+    if (event.code === 'Escape') {
+        instance.close()
+    }
+}
+    
+
+
+
+
