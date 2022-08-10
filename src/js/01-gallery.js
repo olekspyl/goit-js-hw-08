069e04715192941c55eebe0b61fbe5104a3748ad
Oleksandra Pylypchuk <lemocream@gmail.com>
2022-08-10 13:53:14 +0200

    to mentor

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,15 +4,12 @@ import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
 // Change code below this line
 const imgs = galleryItems
     .map(galleryItem => {
-        const itemEl = `<div class="gallery__item">
-        <a class="gallery__link" href="${galleryItem.original}">
-        <img src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" 
-        class="gallery__image"/>
-        </a>
-        </div>` 
+        const itemEl = `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" /></a>` 
         return itemEl;
     })
     .join(" ");
@@ -23,22 +20,19 @@ listEl.insertAdjacentHTML('afterbegin', imgs);
 const galleryContainer = document.querySelector(".gallery");
 galleryContainer.addEventListener('click', onImageClick);
 
-let instance;
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 250,
+
+})
 
 function onImageClick(event) {
     event.preventDefault();
     if (!event.target.classList.contains("gallery__image"))
         return;
-   
-
-     instance = basicLightbox
-         .create(`<img src="${event.target.dataset.source}" width="800" height="600">`,
-             {onShow: (instance) => {window.addEventListener('keydown', onEscPress)},
-            onClose: (instance) => {window.removeEventListener('keydown', onEscPress)}
-})
-
-instance.show()
 }
+
+
 
 galleryContainer.addEventListener('keydown', onEscPress)
 function onEscPress(event) {
@@ -46,4 +40,9 @@ function onEscPress(event) {
         instance.close()
     }
 }
+    
+
+
+
+
 
