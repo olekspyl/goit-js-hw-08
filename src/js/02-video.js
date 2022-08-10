16f48109ad84449245dfcb82a3466ad5e7b5d1ac
Oleksandra Pylypchuk <lemocream@gmail.com>
2022-08-10 13:55:31 +0200

    to-mentor

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const SAVED_TIME = localStorage.getItem("videoplayer-current-time");
+let parsedTime;
+
+function checkTime(SAVED_TIME) {
+if (SAVED_TIME) {
+    parsedTime = JSON.parse(SAVED_TIME);
+} 
+}
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", SAVED_TIME)
+};
+
+checkTime(SAVED_TIME);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+    .setCurrentTime(parsedTime.seconds)
+    .catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const refs = {
+    form: document.querySelector('form'),
+}
+
+const array = {};
+const LOCAL_STORAGE = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormIput);
+
+takeTextInput();
+
+function onFormIput(event) {
+    array[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(array));
+
+    console.log(array);
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCAL_STORAGE);
+}
+    
+function takeTextInput() {
+    const savedText = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+    refs.form.email.value = savedText.email;
+    refs.form.message.value = savedText.message;
+    
+    console.log(savedText.email);
+    console.log(savedText.message);
+    console.log(refs.form.email.value);
+    console.log(refs.form.message.value);
+    console.log(refs.form);
+
+ }
+   
+
+
+
+
+
+
 
